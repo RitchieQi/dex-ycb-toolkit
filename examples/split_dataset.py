f104f0a15e5e05c92c47aecc5f1085e6126e3379
RitchieQi <pinkfloydtq@gmail.com>
2023-08-23 21:08:12 +0100

    add methods about manopth

--- examples/split_dataset.py
+++ examples/split_dataset.py
@@ -14,6 +14,8 @@ import trimesh
 import cv2
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
+from scipy.spatial.transform import Rotation as R
+
 
 
 def center_vert_bbox(vertices, bbox_center=None, bbox_scale=None, scale=False):
@@ -67,20 +69,50 @@ class dexycb():
         self.getdata = DexYCBDataset(setup,split)
         self.filter_no_hand = True
         self.filter_no_contact = True
-        self.filter_threshold = 50
+        self.filter_threshold = 15
         self.use_right_hand = True
         self.split = split
 
     def __len__(self):
         return len(self.getdata)
 
+    def mano_process(self,sample):
+        lable = self.load_label(sample)
+        pose_m = lable['pose_m'].squeeze(0)
+        print(pose_m.shape)
+        global_rot = np.array(R.from_euler('xyz',pose_m[:3]).as_matrix(), dtype=np.float32)
+        global_trans = pose_m[-3:]
+        joint_3d = lable['joint_3d'] - global_trans
+        joint = np.matmul(joint_3d, np.linalg.inv(global_rot))
+        rot1 = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]],dtype=np.float32)
+        rot2 = np.array([[1, 0, 0],[0, 0, 1],[0, -1, 0]],dtype=np.float32)
+        joint = np.matmul(joint, rot1)
+        joint = np.matmul(joint, rot2)
+
+        points = lable['pose_y'][sample['ycb_grasp_ind']][:3,3] - global_trans
+        points = np.matmul(points, np.linalg.inv(global_rot))
+        points = np.matmul(points, rot1)
+        points = np.matmul(points, rot2)
+        return joint,points
     
+    def mano_inverse(self,joint,points,sample):
+        label = self.load_label(sample)
+        pose_m = label['pose_m'].squeeze(0)
+        global_rot = np.array(R.from_euler('xyz',pose_m[:3]).as_matrix(), dtype=np.float32)
+        rot1 = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]],dtype=np.float32)
+        rot2 = np.array([[1, 0, 0],[0, 0, 1],[0, -1, 0]],dtype=np.float32)
+        joint = np.matmul(joint, np.linalg.inv(rot2))
+        joint = np.matmul(joint, np.linalg.inv(rot1))
+        joint = np.matmul(joint, global_rot)
+        joint = joint + pose_m[-3:]
+        
+
+
     def iterate(self):
         #sample = self.getdata[idx]
         objdir = 'data/dexycb/{}/obj_mesh/'.format(self.split)
         handdir = 'data/dexycb/{}/hand_mesh/'.format(self.split)
         metadir = 'data/dexycb/{}/meta/'.format(self.split)
-
         os.makedirs(objdir,exist_ok = True)
         os.makedirs(handdir,exist_ok = True)
         os.makedirs(metadir,exist_ok = True)
@@ -100,6 +132,24 @@ class dexycb():
                 json.dump(sample,open(metadir + '{}.json'.format(count),'w'))
                 
                 count += 1
+
+    def iterate_shadow(self):
+        #hand joints & obj center
+        pass
+
+    def viz(self):
+        if self.filter_no_hand and self.filter_no_contact and self.use_right_hand:
+            for i,sample in tqdm(enumerate(self.getdata),total = self.__len__()):
+                if sample["mano_side"] == 'left':
+                    continue
+                if np.all(self.get_joint2d(sample) == -1.0):
+                    continue
+                if cdist(self.get_obj_verts_transmed(sample),self.get_joint3d(sample)).min()*1000 > self.filter_threshold:
+                    continue
+                joint,center = self.mano_process(sample)
+                print(center)
+                print(joint)
+                self.plot(sample)
 
     def load_label(self,sample):
         label = np.load(sample['label_file'])
@@ -170,14 +220,50 @@ class dexycb():
         new_mesh = trimesh.Trimesh(vertices=mesh.vertices, faces=mesh.faces)
         return new_mesh
 
-    def get_camera(self,idx,scene):
-        fx = self.getdata[idx]['intrinsics']['fx']
-        fy = self.getdata[idx]['intrinsics']['fy']
-        cx = self.getdata[idx]['intrinsics']['ppx']
-        cy = self.getdata[idx]['intrinsics']['ppy']
+    def get_camera(self,sample,scene):
+        fx = sample['intrinsics']['fx']
+        fy = sample['intrinsics']['fy']
+        cx = sample['intrinsics']['ppx']
+        cy = sample['intrinsics']['ppy']
         cam = pyrender.IntrinsicsCamera(fx, fy, cx, cy)
         scene.add(cam, pose=np.eye(4))
         return None
+    
+    def opencv_camera(self,sample):
+        fx = sample['intrinsics']['fx']
+        fy = sample['intrinsics']['fy']
+        cx = sample['intrinsics']['ppx']
+        cy = sample['intrinsics']['ppy']
+        intrinsics = np.array([[fx, 0, cx],
+                               [0, fy, cy],
+                               [0, 0,  1 ]])
+        rotation = np.eye(3)
+        translation = np.zeros(3)
+        points = self.load_label(sample)['pose_y'][sample['ycb_grasp_ind']][:3,3]
+        points = points.reshape(3,1)
+        points_2d = cv2.projectPoints(points,rotation,translation,intrinsics,None)[0]
+        print(points_2d)
+        return points_2d
+    
+    def _test_mano_process(self,sample):
+        fx = sample['intrinsics']['fx']
+        fy = sample['intrinsics']['fy']
+        cx = sample['intrinsics']['ppx']
+        cy = sample['intrinsics']['ppy']
+        intrinsics = np.array([[fx, 0, cx],
+                               [0, fy, cy],
+                               [0, 0,  1 ]])
+        rotation = np.eye(3)
+        translation = np.zeros(3)
+        joint,points = self.mano_process(sample)
+
+
+
+
+
+        joint_2d = cv2.projectPoints(joint,rotation,translation,intrinsics,None)[0]
+        points_2d = cv2.projectPoints(points,rotation,translation,intrinsics,None)[0]
+        return joint_2d,points_2d      
     
     def get_label(self,idx):
         label = np.load(self.getdata[idx]['label_file'])
@@ -261,13 +347,14 @@ class dexycb():
 
         # render image
         im_render, _ = render.render(scene_im)
-        im_real = cv2.imread(self.getdata[idx]['color_file'])
+        im_real = cv2.imread(idx['color_file'])
         im_real = im_real[:, :, ::-1]
         im = 0.33 * im_real.astype(np.float32) + 0.67 * im_render.astype(np.float32)
         im = im.astype(np.uint8)
 
         print('Close the window to continue.')
-
+        point = self.opencv_camera(idx)
+        cv2.circle(im,(int(point[0,0,0]),int(point[0,0,1])),5,(0,0,255),-1)
         plt.imshow(im)
         plt.tight_layout()
         plt.show()
@@ -327,11 +414,14 @@ def creat_dataset():
 
 if __name__ == '__main__':
     #creat_dataset()
+    # dex_train = dexycb('s1', 'train')
+    # dex_train.iterate()
+    
+    # dex_test = dexycb('s1', 'test')
+    # dex_test.iterate()
+    
     dex_train = dexycb('s1', 'train')
-    dex_train.iterate()
-    
-    dex_test = dexycb('s1', 'test')
-    dex_test.iterate()
-    
+    dex_train.viz()
+    #dex_train.opencv_camera(dex_train.getdata[0])
 
 
